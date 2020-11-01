16fae93e8510be3148b2224e497a140c9d3e6a64
Krzysztof Sordyl <krzysztof.sordyl@tsh.io>
2020-11-01 10:39:36 +0100

    [NRA-2] add example get and post request with next api

--- pages/admin/products/index.js
+++ pages/admin/products/index.js
@@ -1,16 +1,81 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+import { useState } from "react";
+const prisma = new PrismaClient();
 
-export async function getServerSideProps(context) {
-  const products = await prisma.product.findMany()
+export async function getServerSideProps() {
+  const products = await prisma.product.findMany();
+  const categories = await prisma.category.findMany();
   return {
-    props: { products }
-  }
+    props: { products, categories },
+  };
 }
 
-export default function AdminProductsPage ({ products }) {
-  console.log(products)
+export default function AdminProductsPage({ products, categories }) {
+  const DEFAULT_CATEGORY = categories[0].name;
+  const [form, setForm] = useState({
+    name: "",
+    price: "",
+    category: DEFAULT_CATEGORY,
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch("/api/admin/products/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: form.name,
+        category: form.category,
+        price: form.price,
+      }),
+    });
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
   return (
-    <div>Products page</div>
-  )
+    <div>
+      <h1>List of available products</h1>
+      <h2>Add new product</h2>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              <h3>{product.name}</h3>
+              <p>{product.priceCents}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Product name
+          <input
+            name="name"
+            type="text"
+            placeholder="Product name"
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Product price in cents
+          <input
+            name="price"
+            type="text"
+            placeholder="Product price in cents"
+            onChange={handleChange}
+          />
+        </label>
+        <select name="category" onChange={handleChange}>
+          {categories.map((category) => (
+            <option key={category.id}>{category.name}</option>
+          ))}
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
